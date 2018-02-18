847a28993c8a5020a59624725a3ccf6d599e75ec
afrizalrizkypp <afrizalrizkypp@gmail.com>
2018-02-18 23:41:59 +0700

    push pertama kali

---
+++ app/src/main/java/com/example/android/afrizalrpp_1202151232_modul2/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.example.android.afrizalrpp_1202151232_modul2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
